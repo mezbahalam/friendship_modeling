49f93436e2e327bb3e89fda133cb024914358692
mezbahalam <mezbahalam26@gmail.com>
2015-01-19 18:45:00 +0600

    friendship model ready

--- test/models/user_friendship_test.rb
+++ test/models/user_friendship_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class UserFriendshipTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  should belong_to(:user)
+  should belong_to(:friend)
+
+
+  test "that creating a friendship works without raisind an exception" do
+    assert_nothing_raised do
+      UserFriendship.create user: users(:mezbah), friend: users(:mez)
+    end
+  end
+
+  test "that creating a friendship based on user id and friend id works" do
+    UserFriendship.create user_id: users(:mezbah), friend: users(:mez)
+    assert users(:mezbah).friends.include?(users(:mez))
+  end
 end
