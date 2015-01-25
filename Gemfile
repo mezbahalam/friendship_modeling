015057e68f5dd2d27f83ca4f2a1dc9951445f1a1
mezbahalam <mezbahalam26@gmail.com>
2015-01-25 14:21:02 +0600

    test problem in new and edit

--- Gemfile
+++ Gemfile
@@ -1,10 +1,12 @@
 source 'http://rubygems.org'
 
+
+
 gem 'rails', '4.1.8'
 
   group :test do
     gem 'shoulda'
-    gem 'better_errors'
+    gem 'factory_girl_rails'
   end
 
   group :development, :test do
@@ -18,19 +20,18 @@ gem 'rails', '4.1.8'
   end
 
 
-  group :assets do
-    gem 'sass-rails', '~> 4.0.3'
-    gem 'uglifier', '>= 1.3.0'
-    gem 'coffee-rails', '~> 4.0.1'
-    gem 'jquery-rails'
-    gem 'turbolinks'
 
-  end
-
+gem 'sass-rails', '~> 4.0.3'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.1'
+gem 'jquery-rails'
+gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'tzinfo-data', platforms: [:mingw, :mswin]
 
+
 gem 'devise'
 gem 'simple_form'
+gem 'better_errors'
 gem 'state_machine'
--- test/models/user_friendship_test.rb
+++ test/models/user_friendship_test.rb
@@ -12,13 +12,68 @@ class UserFriendshipTest < ActiveSupport::TestCase
   end
 
   test "that creating a friendship based on user id and friend id works" do
-    UserFriendship.create user_id: users(:mezbah), friend: users(:mez)
-    assert users(:mezbah).friends.include?(users(:mez))
+    UserFriendship.create user_id: users(:mezbah).id, friend_id: users(:mez).id
+    assert users(:mezbah).pending_friends.include?(users(:mez))
   end
 
   context "a new instance" do
     setup do
-      UserFriendship.new user: users(:mezbah), friend: users(:mez)
+      @user_friendship = UserFriendship.new user: users(:mezbah), friend: users(:mez)
+    end
+
+    should "have a pending state" do
+      assert_equal 'pending', @user_friendship.state
     end
   end
+
+  context "#send_request_email" do
+
+    setup do
+      @user_friendship = UserFriendship.create user: users(:mezbah), friend: users(:mez)
+    end
+
+    should "send an email" do
+      assert_difference 'ActionMailer::Base.deliveries.size', 1 do
+        @user_friendship.send_request_email
+      end
+    end
+  end
+
+  context "#accept!" do
+    setup do
+      @user_friendship = UserFriendship.create user: users(:mezbah), friend: users(:mez)
+    end
+
+    should "set the state to accepted" do
+      @user_friendship.accept!
+      assert_equal "accepted", @user_friendship.state
+    end
+
+    should "send an acceptance email" do
+      assert_difference 'ActionMailer::Base.deliveries.size', 1 do
+        @user_friendship.accept!
+      end
+    end
+
+    should "include the friend in the list of friends" do
+      @user_friendship.accept!
+      users(:mezbah).friends.reload
+      assert users(:mezbah).friends.include?(users(:mez))
+    end
+  end
+
+  context ".request" do
+    should "create two user friendships" do
+      assert_difference 'UserFriendship.count', 2 do
+        UserFriendship.request(users(:mezbah), users(:mez))
+      end
+    end
+
+    should "send a friend request email" do
+      assert_difference 'ActionMailer::Base.deliveries.size', 1 do
+        UserFriendship.request(users(:mezbah), users(:mez))
+      end
+    end
+  end
+
 end
